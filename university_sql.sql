c372615587e7926c3589a88678b699e179de580d
aswinitiral <k.aswinii@gmail.com>
2022-03-10 15:26:59 -0500

    first commit

---
+++ university_sql.sql
@@ -0,0 +1,18 @@
+
+drop table university cascade;
+
+
+CREATE TABLE university
+(
+country varchar,
+state varchar,
+name varchar,
+webpage varchar
+);
+
+select * from university limit 10;
+
+select * from university
+where name = 'Xavier University'
+
+
